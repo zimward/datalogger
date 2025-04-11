a1a6f1a621dbf586d7aefd89d507c7539e143e83
zimward <zimward@zimward.moe>
2025-04-11 09:27:30 +0200

    implemented writing values to sdcard

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,13 @@ use core::sync::atomic::Ordering::Relaxed;
 
 use avg::Avg;
 use cortex_m::singleton;
-use cortex_m_semihosting::hprintln;
 
 use cortex_m_rt::entry;
 
-use embedded_hal::spi::{self, Mode};
-use embedded_sdmmc::{BlockDevice, Directory, SdCard, VolumeIdx, VolumeManager};
+use embedded_hal::spi::Mode;
+use embedded_sdmmc::{BlockDevice, File, SdCard, TimeSource, VolumeIdx, VolumeManager};
 use led::Led;
 use sdcard::{sderror, FakeTimeSource};
-use serde::Deserialize;
-use serde_csv_core::csv_core;
 use stm32f1xx_hal::adc::{Adc, SampleTime, SetChannels};
 use stm32f1xx_hal::dma::Half;
 use stm32f1xx_hal::gpio::{Analog, PA0, PA1};
@@ -44,6 +41,7 @@ fn get_millis() -> u32 {
 //slowest sample time for adc
 const SAMPLE_TIME: SampleTime = SampleTime::T_239;
 const SAMPLE_FREQ: u32 = 8_000_000 / 239; // 12MHz adc clock, 1.5 cycle per conversion averaged over 239 cycle
+                                          // 8E6 = 12E6 / 1.5
 
 #[derive(serde::Deserialize)]
 struct Config {
@@ -72,6 +70,26 @@ PB0 start/stop -- in dokumentation nach remap möglichkeit schauen.
 
 #######################
 */
+
+fn save<D, T, P>(data: &[u8], file: File, vol_mgr: &mut VolumeManager<D, T>, led: &mut Led<P>)
+where
+    T: TimeSource,
+    D: BlockDevice,
+    P: FnMut(u16),
+{
+    let mut offset: usize = 0;
+    loop {
+        let res = vol_mgr
+            .write(file, &data[offset..])
+            .unwrap_or_else(|_| sderror(led));
+        offset += res;
+        if offset == data.len() {
+            //write finished
+            break;
+        }
+    }
+}
+
 #[allow(non_snake_case, clippy::similar_names, clippy::too_many_lines)]
 #[entry]
 fn main() -> ! {
@@ -166,8 +184,8 @@ fn main() -> ! {
     let root = volume_mgr
         .open_root_dir(vol0)
         .unwrap_or_else(|_| sderror(&mut led));
-    let mut config = volume_mgr
-        .open_file_in_dir(root, "config.ini", embedded_sdmmc::Mode::ReadOnly)
+    let config = volume_mgr
+        .open_file_in_dir(root, "config.csv", embedded_sdmmc::Mode::ReadOnly)
         .unwrap_or_else(|_| sderror(&mut led));
     let mut buffer = [0u8; 512];
     let mut reader = serde_csv_core::Reader::<32>::new();
@@ -191,6 +209,14 @@ fn main() -> ! {
     let cfg = cfg.unwrap_or_else(|| {
         sderror(&mut led);
     });
+
+    let outfile = volume_mgr
+        .open_file_in_dir(
+            root,
+            "out.csv",
+            embedded_sdmmc::Mode::ReadWriteCreateOrTruncate,
+        )
+        .unwrap_or_else(|_| sderror(&mut led));
     //LED config
 
     //system timer setup
@@ -215,10 +241,12 @@ fn main() -> ! {
         unsafe { b.unwrap_unchecked() }
     };
     let mut adc_buffer = adc.circ_read(dma_buffer);
-    let mut last_half = Half::First;
+    let mut last_half = Half::Second;
 
     let mut channel_a_avg = Avg::new(cfg.ms_per_sample * SAMPLE_FREQ / 1000);
     let mut channel_b_avg = Avg::new(cfg.ms_per_sample * SAMPLE_FREQ / 1000);
+
+    let mut writer = serde_csv_core::Writer::new();
 
     loop {
         //100 Hz loop for uncritical purposes (LED)
@@ -241,6 +269,12 @@ fn main() -> ! {
                                 .map(|v| convert(cfg.factor_per_ma, v));
                             if let (Some(a), Some(b)) = (a, b) {
                                 let pair = DataPair(a, b);
+                                //one line should not be larger than 16 digits so this is enough margin
+                                let mut buf = [0u8; 32];
+                                if let Ok(size) = writer.serialize(&pair, &mut buf) {
+                                    //write to disk
+                                    save(&buf[..size], outfile, &mut volume_mgr, &mut led);
+                                }
                             }
                         }
                     } //else overrun
